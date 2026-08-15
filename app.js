e3748696711e447d607a4dd6eea2a4b494bc819e
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2026-08-15 11:26:35 +0530

    Operator

--- app.js
+++ app.js
@@ -1,55 +1,53 @@
-let a = 10;
-let b = 20;
+console.log(Math.floor(10/3)) //has to a be an integer value
+console.log(3/10)
+console.log(10%3)
+console.log(2%10)  // smaller one is the reminder
 
-console.log(a + b); //arthematic operation
-
-let s1 = "hello";
-let s2 = 10;
-console.log(typeof (s1 + s2)); //concatenation
-
-console.log("The sum of " + a + "and " + b + " is " + a + b);
-console.log("The sum of " + a + "and " + b + " is " + (a + b));
-console.log(a + b + " The sum of " + a + " and " + b + " is ");
-
-//Js start reading code from left
-//in check the operater and both side value and if its interger then the whole is integer , and in first if any one is integer then it is integer
-
-console.log("1" + 1); // because + have two works concatanation and arthematic
-console.log("1" - 1); //but - only have one work
-
-//type coercion - change the type in runtime because sign dont have any other mean
-
-// let prompt = require('prompt-sync')() //aslways give no. in string
-
-// let age = Number(prompt("Enter your age "))  //convert string to numb
-
-// console.log(typeof age);
-// console.log(`Age ${age}`);
-
-//Swap values
-
-let c = 10;
-let d = 20;
-console.log(c);
-console.log(d);
-
-let temp = c;
-c = d;
-d = temp;
-
-console.log(c);
-console.log(d);
+// n = 45678 now if want to remove last digit divide by 10 
+// if two digit divide by 100
+// if three digit divide by 1000
 
 
-let x = 10
-let y = 20
-console.log(x,y);
+// 45678 want last 1 digit mod % with 10 and continur like divide
+
+console.log(456878%1000)
+
+// Relational Operaters
+
+console.log(10>9)
+
+console.log(10>=9) // true cause 1 condition 
+
+console.log("9" == 9) //true because matching values not type
+
+console.log("9" === 9) //false because matching values and type both
+
+//Logical operater
+
+//AND
+
+console.log(12>7 && 8<56 && 5>43)  //both condition must be true
+//in and if first is false it doest checjk others number
+
+// OR
+//Doest matter if one value is true answer is true
+console.log(12>3 || 43>21 || 43>543)
+
+// Not = reverse
 
 
-x = x+y
-y = x - y
-x = x - y
-console.log(x,y);
+//Unary Operater
 
-[x,y] = [y,x]
-console.log(x,y);
+// post = dhaba (use then pay) , pre  = dominose (pay then use)
+
+// let x = 10 
+// console.log(x , y)
+//  y = ++x //post
+// console.log(x , y)
+
+let y = 7;
+y += 2;
+y *= 3;
+y -= 4;
+y %= 5;
+console.log(y);
