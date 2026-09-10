939e0973887a8f84c50cc11cabc9ea30f6c0a742
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2026-09-10 12:43:08 +0530

    Conditionals

---
+++ 05_conditionals.js
@@ -0,0 +1,64 @@
+let prompt = require('prompt-sync')();
+
+// let age = Number(prompt("enter your age "))
+
+// if(age>18){
+//     console.log("valid voter")
+// }
+// else{
+//     console.log("bhakk");
+    
+// }
+
+// let marks = Number(prompt("Enter Your marks "))
+
+// if (marks>85 && marks<=95) console.log("excellent");
+// else if (marks>80 && marks<=85) console.log("very good");
+// else if (marks>70 && marks<=80) console.log("Good");
+// else console.log("fair");
+
+
+// let number1 = Number(prompt("Enter number "))
+// let number2 = Number(prompt("Enter number "))
+
+// if(number1>number2)console.log(`${number1} is greatest`);
+// else console.log(`${number2} is greatest`);
+
+// let num = Number(prompt("Enter number to check odd even "))
+// if(num%2 == 0)console.log(`${num} is even`);
+// else console.log(`${num} is odd`);
+
+
+// let a = Number(prompt("Enter a "))
+// let b = Number(prompt("Enter b "))
+// let c = Number(prompt("Enter c "))
+
+// if(a>b && a>c) console.log("a is greater")
+// else if (b > a && b > c) console.log("b is greater");
+// else if (c >a && c > b) console.log("c is greater");
+
+// let year = Number(prompt("Enter Number "))
+
+// if(year% 4==0 && year%100 != 0) console.log("This is leap year")
+// else if(year% 400==0) console.log("This is leap year");
+// else console.log("not leap year");
+    
+
+// let amount = Number(prompt('enter amount '))
+// let payableamount = 0
+// let dis = 0
+
+// if(amount>0 && amount<=5000) {
+//    dis = 1
+// }
+
+// else if(amount>5000 && amount<=7000) {
+//     dis = 5
+// }
+
+// else if(amount>7000 && amount<=9000) {
+//     dis = 10
+// }
+// payableamount = amount - (dis * amount) / 100;
+// console.log("payable amount " + payableamount)
+
--- app.js
+++ app.js
@@ -1,53 +1,66 @@
-console.log(Math.floor(10/3)) //has to a be an integer value
-console.log(3/10)
-console.log(10%3)
-console.log(2%10)  // smaller one is the reminder
+// let prompt = require("prompt-sync")();
 
-// n = 45678 now if want to remove last digit divide by 10 
-// if two digit divide by 100
-// if three digit divide by 1000
+// let p = prompt("Enter Priciple ")
+// let r = prompt("Enter r ")
+// let t = prompt("Enter t ")
 
+// console.log((p* Math.pow(1 + (r/100),t) - p).toFixed(2))
 
-// 45678 want last 1 digit mod % with 10 and continur like divide
+console.log(Math.trunc((Math.random()*9000)+1000)) //OTP
 
-console.log(456878%1000)
+console.log(Math.abs(-10)) 
 
-// Relational Operaters
-
-console.log(10>9)
-
-console.log(10>=9) // true cause 1 condition 
-
-console.log("9" == 9) //true because matching values not type
-
-console.log("9" === 9) //false because matching values and type both
-
-//Logical operater
-
-//AND
-
-console.log(12>7 && 8<56 && 5>43)  //both condition must be true
-//in and if first is false it doest checjk others number
-
-// OR
-//Doest matter if one value is true answer is true
-console.log(12>3 || 43>21 || 43>543)
-
-// Not = reverse
+//convert value to positive
 
 
-//Unary Operater
 
-// post = dhaba (use then pay) , pre  = dominose (pay then use)
+console.log(Math.ceil(10.4))
 
-// let x = 10 
-// console.log(x , y)
-//  y = ++x //post
-// console.log(x , y)
+//round figure the number by increasing it , accept float
 
-let y = 7;
-y += 2;
-y *= 3;
-y -= 4;
-y %= 5;
-console.log(y);
+
+
+
+console.log(Math.floor(10.4))
+
+//round figure the number by decresing it , accept float
+
+
+
+
+console.log(Math.round(10.4))
+
+//act like seal when more the .5 or 5 , act as floore when less then 5
+
+
+
+
+console.log(Math.trunc(13.743675437))
+
+//Remove decimal
+
+
+
+
+console.log(Math.pow(10,3))
+
+//power accept two parameter , kiske upar aur kitni power
+
+
+
+
+console.log(Math.max(32,45,6778,342,23,547,87))
+
+//give max value same min
+
+
+
+
+console.log(Math.trunc(Math.random()*20))
+
+//random value
+
+
+
+
+ // toFixed get the decimal point we need
